a7ae1278b88c984676ea940ebaf01659a09dead3
joaopedropiraja <jplp100@hotmail.com>
2023-05-11 23:39:25 -0300

    video 2

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,39 @@
-import { Container } from "./Styles";
+import { useState } from "react";
+import { Container, Form, Campo, Label, Input, Button } from "./Styles";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const handleSumbit = async (e) => {
+    e.preventDefault();
+    console.log({ email, senha });
+  };
   return (
     <Container>
-      <h1>LOGIN</h1>
+      <Form onSubmit={handleSumbit}>
+        <Campo>
+          <Label htmlFor="email">E-mail: </Label>
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Campo>
+        <Campo>
+          <Label htmlFor="senha">Senha: </Label>
+          <Input
+            type="password"
+            name="senha"
+            id="senha"
+            required
+            onChange={(e) => setSenha(e.target.value)}
+          />
+        </Campo>
+        <Button type="submit">Entrar</Button>
+      </Form>
     </Container>
   );
 }
--- src/pages/Login/Styles.js
+++ src/pages/Login/Styles.js
@@ -6,3 +6,22 @@ export const Container = styled.div`
   align-items: center;
   height: 100%;
 `;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  gap: 15px;
+`;
+export const Campo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+export const Label = styled.label``;
+export const Input = styled.input``;
+export const Button = styled.button`
+  padding: 5px;
+  width: 80px;
+`;
